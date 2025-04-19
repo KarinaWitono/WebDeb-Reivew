705a4245a515d39464928b1e4a62bff444e2f30f
KarinaWitono <karina.a.witono@gmail.com>
2025-04-18 20:00:40 -0500

    new changes

--- warmup_1/script.js
+++ warmup_1/script.js
@@ -5,12 +5,13 @@ var button = document.getElementById("button-1");
 var canvas = document.getElementById('canvas-1');
 var ctx = canvas.getContext('2d');
 var heading = document.getElementById('heading-1')
+var colors = "red"; 
 
 canvas.width = 150;
 canvas.height = 150;
 
 ctx.beginPath();
-ctx.fillStyle = "red";
+ctx.fillStyle = colors;
 ctx.fillRect(75, 75, 50, 30);
 ctx.closePath();
 
@@ -18,4 +19,11 @@ button.onclick = background;
 
 function background () {
     heading.innerHTML = 'Used as the title of a project';
+    rectangle()
+}
+
+function rectangle () {
+    colors = "blue";
+    ctx.fillStyle = colors;
+    ctx.fillRect(75, 75, 50, 30);
 }
